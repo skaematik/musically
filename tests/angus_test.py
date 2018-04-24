6536fda4f5bc1277167930113439b2af403ef10e
Angus Fletcher <a.m.fletcher1994@gmail.com>
2018-04-24 23:55:41 +1000

    added detection of each line in music

--- tests/angus_test.py
+++ tests/angus_test.py
@@ -4,30 +4,37 @@ import cv2
 import numpy as np
 
 from utils import *
-
+from skimage.feature import peak_local_max
+from skimage.morphology import watershed
+from scipy import ndimage
+import matplotlib.pyplot as plt
 
 def main():
-    img = import_img_greyscale('./sheets/oneliner.png')
+    img = import_img_greyscale('./sheets/ode-to-joy.png')
     cv2.imshow('pre', img)
-    thresh_img = cv2.adaptiveThreshold(inv(img,255),255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,31,0)
-    out = inv(cv2.morphologyEx((thresh_img), cv2.MORPH_RECT, np.ones((3, 1), np.uint8)), 255)
+    thresh_img = inv(cv2.adaptiveThreshold(inv(img,255),255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,31,0))
+    out = thresh_img # inv(cv2.morphologyEx((thresh_img), cv2.MORPH_RECT, np.ones((3, 1), np.uint8)), 255)
     cv2.imshow('post', out)
-    runFinder = inv(thresh_img,255)
-    cv2.imshow('run found on', runFinder)
-    staff_width, staff_height = calculate_staff_values(runFinder)
-    print('width: {} height: {}'.format(staff_width, staff_height))
-    out = inv(cv2.morphologyEx(inv(out,255), cv2.MORPH_DILATE, np.ones((2, 2), np.uint8)), 255)
-    out = inv(cv2.morphologyEx(inv(out,255), cv2.MORPH_DILATE, np.ones((2, 2), np.uint8)), 255)
-    out = inv(cv2.morphologyEx(inv(out,255), cv2.MORPH_DILATE, np.ones((2, 2), np.uint8)), 255)
-    out = inv(cv2.morphologyEx(inv(out,255), cv2.MORPH_DILATE, np.ones((2, 2), np.uint8)), 255)
-    out = inv(cv2.morphologyEx(inv(out, 255), cv2.MORPH_ERODE, np.ones((2, 2), np.uint8)), 255)
-    out = inv(cv2.morphologyEx(inv(out, 255), cv2.MORPH_ERODE, np.ones((2, 2), np.uint8)), 255)
-    out = inv(cv2.morphologyEx(inv(out, 255), cv2.MORPH_OPEN, np.ones((3, 3), np.uint8)), 255)
-    # out = inv(cv2.morphologyEx(inv(out, 255), cv2.MORPH_OPEN, np.ones((5, 5), np.uint8)), 255)
-    # out = inv(cv2.morphologyEx(inv(out, 255), cv2.MORPH_OPEN, np.ones((5, 5), np.uint8)), 255)
-    cv2.imshow('post2', out)
-
-    cv2.waitKey(0)
+    find_staffs(out,show_plots=True)
+    # D = ndimage.distance_transform_edt((out))
+    # plt.imshow(D, interpolation='nearest')
+    # plt.show()
+    # cv2.imshow('Dist', (D).astype(np.uint8))
+    # cv2.imshow('Dist on', (out))
+    # runFinder = inv(thresh_img,255)
+    # cv2.imshow('run found on', runFinder)
+    # staff_width, staff_height = calculate_staff_values(runFinder)
+    # print('width: {} height: {}'.format(staff_width, staff_height))
+    # out = inv(cv2.morphologyEx(inv(out,255), cv2.MORPH_DILATE, np.ones((2, 2), np.uint8)), 255)
+    # out = inv(cv2.morphologyEx(inv(out,255), cv2.MORPH_DILATE, np.ones((2, 2), np.uint8)), 255)
+    # out = inv(cv2.morphologyEx(inv(out,255), cv2.MORPH_DILATE, np.ones((2, 2), np.uint8)), 255)
+    # out = inv(cv2.morphologyEx(inv(out,255), cv2.MORPH_DILATE, np.ones((2, 2), np.uint8)), 255)
+    # out = inv(cv2.morphologyEx(inv(out, 255), cv2.MORPH_ERODE, np.ones((2, 2), np.uint8)), 255)
+    # out = inv(cv2.morphologyEx(inv(out, 255), cv2.MORPH_ERODE, np.ones((2, 2), np.uint8)), 255)
+    # out = inv(cv2.morphologyEx(inv(out, 255), cv2.MORPH_OPEN, np.ones((3, 3), np.uint8)), 255)
+    # # out = inv(cv2.morphologyEx(inv(out, 255), cv2.MORPH_OPEN, np.ones((5, 5), np.uint8)), 255)
+    # # out = inv(cv2.morphologyEx(inv(out, 255), cv2.MORPH_OPEN, np.ones((5, 5), np.uint8)), 255)
+    # cv2.imshow('post2', out)
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from collections import Counter
 import cv2
 import numpy as np
+from scipy import ndimage
+import matplotlib.pyplot as plt
 
 
 def import_img_greyscale(file_name):
@@ -13,7 +15,7 @@ def import_img_greyscale(file_name):
     return img
 
 
-def inv(img, val):
+def inv(img, val=255):
     """ Inverts image
         :arg img
             image to invert
@@ -21,6 +23,40 @@ def inv(img, val):
             maximum value in image
         :returns inverted image"""
     return 255-img
+
+
+def find_staffs(img, show_plots=False, top_buffer=20, left_buffer=20, min_width=0.8, display_for=10000):
+    """Finds each line of music in the img and returns each in a list
+    Steps:
+    1) dialate the image by same width as staff height to ensure connectedness
+    2) apply connected component algorithm
+    3) segment into separate images any labeled image that is at least min_width
+    fraction of total width long.
+
+    :param img: binary image, staff needs to be (roughly) horizontal, make sure background is white
+    :param show_plots: where or not to show the results, will pause until keypress
+    :param display_for: length of time to display plots for
+    :return: a list of np.array for each staff detected. will attempt to center each one with
+     top_buffer and left_buffer whitespace.
+    """
+    staff_width, staff_height = calculate_staff_values(img)
+    out_erroded = cv2.erode(img, np.ones((staff_height, staff_height)))
+    cv2.imshow('image segmentation dialated',out_erroded)
+    labeled_image, num_features = ndimage.label(inv(out_erroded))
+    # Find the location of all objects
+    objs = ndimage.find_objects(labeled_image)
+    # Get the height and width
+    imgs = []
+    for ob in objs:
+        if (ob[1].stop - ob[1].start) > min_width*img.shape[0]:
+            imgs.append(img[ob])
+            if show_plots:
+                cv2.imshow('image segment {}'.format(len(imgs)), imgs[-1])
+    if show_plots:
+        plt.imshow(labeled_image)
+        plt.show()
+        cv2.waitKey(display_for)
+    return imgs
 
 
 def calculate_staff_values(img):
@@ -34,7 +70,6 @@ def calculate_staff_values(img):
         :returns staff_width, staff_height"""
     blackruns = Counter()
     whiteruns = Counter()  # key is run length val is count of this run length
-    count = 0
     is_white = True
     for x in range(img.shape[1]):
         count = 0
@@ -50,4 +85,5 @@ def calculate_staff_values(img):
             count += 1
     print('blackRun: {}', blackruns.most_common(1))
     print('whiteRun: {}', whiteruns.most_common(1))
-    return blackruns.most_common(1), whiteruns.most_common(1)
+    return blackruns.most_common(1)[0][0], whiteruns.most_common(1)[0][0]
+
