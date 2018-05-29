246bd3c575a0ce23d4e8101cb2629e2cf1e75200
Annie Zhang <a.y.zhang@hotmail.com>
2018-05-30 01:32:39 +1000

    server changes

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 import argparse
 from flask import Flask, request, jsonify
+from flask_dropzone import Dropzone
+import os
+import os.path as osp
+import json
+
 app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    DROPZONE_ALLOWED_FILE_TYPE='image',
+    DROPZONE_MAX_FILE_SIZE=1,
+    DROPZONE_MAX_FILES=1000,
+)
+if not os.path.exists(app.config['UPLOADED_PATH']):
+    os.makedirs(app.config['UPLOADED_PATH'])
+
+dropzone = Dropzone(app)
+
 # TODO: do we really need CORS?
 
 from music21 import vexflow, note
+
+FOLDER_MXL = 'mxl'
+EXTENSION_MXL = '.musicxml'
 
 
 @app.route("/")
@@ -11,28 +33,55 @@ def home():
     return "Hello world!"
 
 
-@app.route("/musically")
-def musically():
-    text = request.args.get('query', '')
+# @app.route("/musically")
+# def musically():
+#     # text = request.args.get('query', '')
 
-    print(request)
-    
-    # will call the actual function here
+#     print(request)
 
-    # dummy testing
-    n = note.Note('C#4')
-    to_be_sent = vexflow.toMusic21j.fromObject(n, mode='html')
-    print(to_be_sent)
+#     # will call the actual function here
 
-    return jsonify({
-        'to_be_sent': to_be_sent
-    })
+#     # dummy testing
+#     n = note.Note('C#4')
+#     to_be_sent = vexflow.toMusic21j.fromObject(n, mode='html')
+#     print(to_be_sent)
+
+#     return jsonify({
+#         'to_be_sent': to_be_sent
+#     })
+
+
+# retrieves a sample music xml that is stored in /mxl
+@app.route("/get_music_xml")
+def get_music_xml():
+    sheet = request.args['sheet_name']
+    fn = osp.join(FOLDER_MXL, sheet)
+    fn = fn + EXTENSION_MXL if ~fn.endswith(EXTENSION_MXL) else fn
+    handle = open(fn, 'r', encoding='utf-8')
+    data = handle.read().replace('\n', '')
+    handle.close()
+
+    return jsonify(data)
+
+
+# uploads an image to /uploads
+@app.route("/image_upload", methods=['GET', 'POST'])
+def image_upload():
+    print('received file')
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    else:
+        return json.dumps({'success': False}), 200, {'ContentType': 'application/json'}
+    # return render_template('index.html')
 
 
 @app.after_request
 def add_ua_compat(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers['Access-Control-Allow-Headers'] = 'Cache-Control, X-Requested-With'
     return response
 
 
